87c5ce97134dac41560fc13db740bae6c30bc796
yongda1028 <yundayoung1028@gmail.com>
2020-08-24 21:11:05 +0900

    초단기실황/초단기예보/동네예보 weather api 작성

--- weather/views.py
+++ weather/views.py
@@ -3,12 +3,24 @@ from weather.weatherAPI import *
 
 
 def show_weather(request):
-    weather_info = get_ultra_srt_ncst()
-    weather_info.update(get_vilage_fcst())
+    # 초단기 예보
+    current_weather_key, hourly_weather = get_ultra_srt_fcst()
+    # 초단기 실황 정보로 업데이트
+    hourly_weather[current_weather_key].update(get_ultra_srt_ncst())
+    #동네 예보 (3시간 단위)
+    three_hourly_weather = get_vilage_fcst()
+
+    print(get_ultra_srt_ncst())
+    for i in hourly_weather.items():
+        print(i)
+    for i in three_hourly_weather.items():
+        print(i)
 
     return render(request, 'weather/weather.html', {
-        'weather_info': weather_info,
+        'weather_list': hourly_weather.items(),
+        'current_weather': hourly_weather[current_weather_key],
     })
+
 
 
 
--- weather/weatherAPI.py
+++ weather/weatherAPI.py
@@ -1,33 +1,6 @@
 from datetime import datetime, timedelta
 import requests
 
-fcst_time_map = {
-    '0': '20',
-    '1': '20',
-    '2': '23',
-    '3': '23',
-    '4': '23',
-    '5': '02',
-    '6': '02',
-    '7': '02',
-    '8': '05',
-    '9': '05',
-    '10': '05',
-    '11': '08',
-    '12': '08',
-    '13': '08',
-    '14': '11',
-    '15': '11',
-    '16': '11',
-    '17': '14',
-    '18': '14',
-    '19': '14',
-    '20': '17',
-    '21': '17',
-    '22': '17',
-    '23': '20',
-}
-
 category_map = {
     'POP': '강수확률',
     'PTY': '강수형태',
@@ -44,7 +17,8 @@ category_map = {
     'VEC': '풍향',
     'WSD': '풍속',
     'T1H': '기온',
-    'RN1': '1시간 강수량'
+    'RN1': '1시간 강수량',
+    'LGT': '낙뢰',
 }
 
 
@@ -61,23 +35,74 @@ def get_ultra_srt_ncst():
         base_time = now.strftime("%H")
 
     url = f'{base_url}?serviceKey={key}&dataType=JSON&numOfRows=36&base_date={base_date}&base_time={base_time}00&nx=61&ny=127'
-    weather_list = requests.get(url).json()['response']['body']['items']['item']
+    result = requests.get(url).json()['response']['body']['items']['item']
 
-    for weather in weather_list:
-        print(weather)
+    current_weather = {}
+    for data in result:
+        current_weather[category_map[data['category']]] = data['obsrValue']
+
+    return current_weather
 
 
-# 동네 예보 -> 3시간 단위 기온 4개 & 가장 가까운 시간 기준 하늘상태/강수확률/강수형태
+# 초단기 예보 조회
+def get_ultra_srt_fcst():
+    now = datetime.now()
+    base_url = 'http://apis.data.go.kr/1360000/VilageFcstInfoService/getUltraSrtFcst'
+    key = 'X5SR1tXGMMIIhiGfESNHl934eVnCDtQwTN%2B7JYgkGs2vWFkDdter5IhoTH8zNSKPuohVnrycbdhG%2F%2B5tHqQBVw%3D%3D'
+    base_date = now.strftime('%Y%m%d')
+    base_time = now - timedelta(hours=1)
+
+    url = f'{base_url}?serviceKey={key}&dataType=JSON&numOfRows=60&base_date={base_date}&base_time={base_time.strftime("%H")}30&nx=61&ny=127'
+    result = requests.get(url).json()['response']['body']['items']['item']
+
+    hourly_weather = {
+        f'{(base_time + timedelta(hours=1)).strftime("%H")}00': {},
+        f'{(base_time + timedelta(hours=2)).strftime("%H")}00': {},
+        f'{(base_time + timedelta(hours=3)).strftime("%H")}00': {},
+        f'{(base_time + timedelta(hours=4)).strftime("%H")}00': {},
+        f'{(base_time + timedelta(hours=5)).strftime("%H")}00': {},
+        f'{(base_time + timedelta(hours=6)).strftime("%H")}00': {},
+    }
+    for data in result:
+        target = hourly_weather[data['fcstTime']]
+        target[category_map[data['category']]] = data['fcstValue']
+
+    CURRENT_WEATHER_KEY = f'{(base_time + timedelta(hours=1)).strftime("%H")}00'
+    return CURRENT_WEATHER_KEY, hourly_weather
+
+
+# 동네 예보 -> 3시간 단위  정보
+NUM_OF_ITEMS = 4    # 총 15개 가져올 수 있음
+MAX_CATEGORY = 14
 def get_vilage_fcst():
     now = datetime.now()
     base_url = 'http://apis.data.go.kr/1360000/VilageFcstInfoService/getVilageFcst'
     key = 'X5SR1tXGMMIIhiGfESNHl934eVnCDtQwTN%2B7JYgkGs2vWFkDdter5IhoTH8zNSKPuohVnrycbdhG%2F%2B5tHqQBVw%3D%3D'
     base_date = now.strftime('%Y%m%d')
-    base_time = fcst_time_map[str(now.hour)]
-    url = f'{base_url}?serviceKey={key}&dataType=JSON&numOfRows=36&base_date={base_date}&base_time={base_time}00&nx=61&ny=127'
-    weather_list = requests.get(url).json()['response']['body']['items']['item']
+    if (now.hour)%3 == 0:
+        base_time = now - timedelta(hours = 4)
+    elif (now.hour)%3 == 1:
+        base_time = now - timedelta(hours = 5)
+    else:
+        base_time = now - timedelta(hours = 3)
+    url = f'{base_url}?serviceKey={key}&dataType=JSON&numOfRows={NUM_OF_ITEMS*MAX_CATEGORY}&base_date={base_date}&base_time={base_time.strftime("%H")}00&nx=61&ny=127'
+    result = requests.get(url).json()['response']['body']['items']['item']
 
-    for weather in weather_list:
-        print(weather)
+    three_hourly_weather = {
+        f'{(base_time + timedelta(hours=4)).strftime("%H")}00': {},
+        f'{(base_time + timedelta(hours=7)).strftime("%H")}00': {},
+        f'{(base_time + timedelta(hours=10)).strftime("%H")}00': {},
+        f'{(base_time + timedelta(hours=13)).strftime("%H")}00': {},
+    }
+    for data in result:
+        target = three_hourly_weather.get(data['fcstTime'])
+        if target == None:
+            break
+        target[category_map[data['category']]] = data['fcstValue']
 
-get_vilage_fcst()
+    return three_hourly_weather
+
+# get_ultra_srt_ncst()
+# get_ultra_srt_fcst()
+# get_vilage_fcst()
+
