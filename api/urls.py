595f6790105a2a2728a1003b654884bbc419087e
neulhan <neulhan@lawandgood.com>
2020-08-30 03:13:38 +0900

    Add word&menu data api

--- api/urls.py
+++ api/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('weather/', views.return_weather, name='return_weather'),
     path('notice/', views.return_notices, name='return_notices'),
     path('suggestion', views.post_suggestion, name='post_suggestion'),
+    path('menulist', views.return_menulist, name='menulist'),
+    path('wordlist', views.return_wordlist, name='wordlist'),
 ]
