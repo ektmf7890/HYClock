4edd0bd27187f7834f062e2a2ccdd62e4631de43
neulhan <neulhan@lawandgood.com>
2020-08-30 00:11:57 +0900

    slack message upgrade

--- api/slackAPI.py
+++ api/slackAPI.py
@@ -2,10 +2,12 @@ import requests
 import json
 from django.conf import settings
 
-def slack(text):
+def slack(title, text):
     url = settings.SECRETS.get("SLACK_CS_WEBHOOK")
     data = {
-        "payload": json.dumps({"text": text})
-        
+        "payload": json.dumps({
+            "pretext": ":loudspeaker: " + title,
+            "text": text    
+        })
     }
     return requests.post(url, data=data)
